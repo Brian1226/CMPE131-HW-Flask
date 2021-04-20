c01dddf462d2d8a604a0a0a42fefbe9f44ff9892
Brian1226 <79072222+Brian1226@users.noreply.github.com>
2021-04-19 20:08:16 -0700

    Updated models.py

--- app/models.py
+++ app/models.py
@@ -1,29 +1,47 @@
 from app import db
 
 class User(db.Model):
-    # have the following columns
-    # id (int)
-    # author (string, unique, can't be null)
-    # message (linkd to Messages table)
-    id = db.Column(db.Integer, primary_key = True)
-    author = db.Column(db.String, unique = True, nullable = False)
-    message = db.relationship('Message', backref 'author')
+    """
+    Holds users' data:
 
+    id (int) = user's unique identification
+    'author' (String) = unique name that lets other users determine who sent the messages
+    'message' = the text that users sent
+    
+    """
+    # id (int)
+    id = db.Column(db.Integer, primary_key = True)
+    # author (string, unique, can't be null)
+    author = db.Column(db.String, unique = True, nullable = False)
+    # message (linkd to Messages table)
+    message = db.relationship('Message', backref 'author', lazy = 'dynamic')
+
+    # outputs "User : " and the string for author right after
+    # ex. User : Brian
     def __repr__(self):
         return f'<User {self.author}>'
-
+        
 class Messages(db.Model):
-    # have the following columns
-    # id (int)
-    # message (string, not unique, can't be null)
-    # user_id link to id (int)
+    """
+    Holds data for all messages:
 
+    id (int) = the message's unique identification
+    'message' (String) = the text that users sent
+    'user_id' (String) = the user's unique identification
+    """
+    
+    # id (int)
+    id = db.Column(db.Integer, primary_key = True)
+
+    # message (string, not unique, can't be null)
+    message = db.Column(db.String, unique = False, nullable = False)
+    
+    # user_id link to id (int)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
     # write __repr__ that outputs
     # <Message: MESSAGE_GOES_HERE>
     # replace MESSAGE_GOES_HERE with the message
-    id = db.Column(db.Integer, primary_key = True)
-    message = db.Column(db.String, unique = False, nullable = False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
+    # ex. Message : Hello, guys!
     def __repr__(self):
-        return '<Messages {}>'.format(self.message)
+        return '<Message {}>'.format(self.message)
