8525ccb2edd25ce4241e931dfb98d7cb113d408a
Brian1226 <79072222+Brian1226@users.noreply.github.com>
2021-04-19 20:59:16 -0700

    Updated routes.py

--- app/routes.py
+++ app/routes.py
@@ -6,26 +6,34 @@ from app.forms import MessageForm
 from app.models import User, Messages
 
 # add route '/' and also add the two methods to handle request: 'GET' and 'POST'
-@app.route("/")
+@app.route('/', methods = ['GET', 'POST'])
 def home():
     form=MessageForm()
     if form.validate_on_submit():
         # check if user exits in database
         # if not create user and add to database
         # create row in Message table with user (created/found) add to ta database
+        user = User.query.filter_by(username = form.username.data).first()
+
+        if user is None:
+            create_user = User(username = form.author.data)
+
+
+
 
     # output all messages
     # create a list of dictionaries with the following structure
     # [{'author':'carlos', 'message':'Yo! Where you at?!'},
     #  {'author':'Jerry', 'message':'Home. You?'}]
+
     posts = [
     {
         'author' : 'Carlos',
-        'message' : 'Yo! Where you at?!'}
+        'message' : 'Yo! Where you at?!'
     },
     {
         'author' : 'Jerry',
-        'messsage' : 'Home. You?'}
+        'message' : 'Home. You?'
     }
 ]
 return render_template('home.html', posts=posts, form=form)
