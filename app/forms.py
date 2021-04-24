a5cea8f809720c17f473c06a0b16f87ed915afb0
Brian1226 <79072222+Brian1226@users.noreply.github.com>
2021-04-24 12:29:54 -0700

    Updated forms.py

--- app/forms.py
+++ app/forms.py
@@ -3,19 +3,19 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField, Integ
 from wtforms.validators import DataRequired
 
 class MessageForm(FlaskForm):
-    """
-    A form that include fields for:
+   """
+   A form that include fields for:
 
-    'Author' (String) = enter the user's name that's writing the message
-    'Message' (String) = enter the user's desired text
-    Submit (button) = a submit button that says 'Send', which will send the message if clicked
-    """
+   'Author' (String) = enter the user's name that's writing the message
+   'Message' (String) = enter the user's desired text
+   Submit (button) = a submit button that says 'Send', which will send the message if clicked
+   """
 
-    # author (string) validator should make this textbox required
-    author = StringField('Author', validators = [DataRequired()])
+   # author (string) validator should make this textbox required
+   author = StringField('Author', validators = [DataRequired()])
 
-    # message (string) validator should make this textbox required
-    message = StringField('Message', validators = [DataRequired()])
+   # message (string) validator should make this textbox required
+   message = StringField('Message', validators = [DataRequired()])
 
-    # submit (button) text should say 'Send'
-    submit = SubmitField('Send')
+   # submit (button) text should say 'Send'
+   submit = SubmitField('Send')
